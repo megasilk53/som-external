56b09fc809fb811cb2940767779ad717822470dd
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2024-04-29 17:30:37 -0400

    wb50nsd_legacy-wbx3: simplify configuration and remove unneeded
    PROD-8256

--- board/wb50nsd_legacy-wbx3/post_image.sh
+++ board/wb50nsd_legacy-wbx3/post_image.sh
@@ -7,42 +7,27 @@ set -x -e
 echo "${BR2_LRD_PRODUCT^^} POST IMAGE script: starting..."
 
 # Tooling checks
-mkimage=${BUILD_DIR}/uboot-custom/tools/mkimage
 mkenvimage=${BUILD_DIR}/uboot-custom/tools/mkenvimage
 
 die() { echo "$@" >&2; exit 1; }
 
-[ -x ${mkimage} ] || \
-	die "No mkimage found (uboot has not been built?)"
 [ -x ${mkenvimage} ] || \
 	die "No mkenvimage found (uboot has not been built?)"
 
-(cd "${BINARIES_DIR}" && "${mkimage}" -f u-boot.scr.its u-boot.scr.itb) || exit 1
-
 ${mkenvimage} -p 0 -s 131072 -o ${BINARIES_DIR}/uboot.env ${BINARIES_DIR}/u-boot-initial-env
 
-echo "# entering ${BINARIES_DIR} for the next command"
-
-(cd ${BINARIES_DIR} && ${mkimage} -f u-boot.its u-boot.itb) || exit 1
-cat "${BINARIES_DIR}/u-boot-spl-nodtb.bin" "${BINARIES_DIR}/u-boot-spl.dtb" > "${BINARIES_DIR}/u-boot-spl.bin"
-
-${mkimage} -T atmelimage -d ${BINARIES_DIR}/u-boot-spl.bin ${BINARIES_DIR}/boot.bin
-
-size_check () {
-	[ $(stat -Lc "%s" ${BINARIES_DIR}/${1}) -le $((${2}*128*1024)) ] || \
-		{ echo "${1} size exceeded ${2} block limit, failed"; exit 1; }
-}
+# Copy mksdcard.sh and mksdimg.sh to images
+ln -rsf ${BR2_EXTERNAL_LRD_SOM_PATH}/board/scripts-common/mksdcard_legacy-wbx3.sh ${BINARIES_DIR}/mksdcard.sh
+ln -rsf ${BR2_EXTERNAL_LRD_SOM_PATH}/board/scripts-common/mksdimg_legacy-wbx3.sh ${BINARIES_DIR}/mksdimg.sh
 
 if [ -n "${VERSION}" ]; then
-	RELEASE_FILE="${BINARIES_DIR}/${BR2_LRD_PRODUCT}-laird-${VERSION}.tar"
+	RELEASE_FILE="${BINARIES_DIR}/${BR2_LRD_PRODUCT}-laird-${VERSION}.tar.bz2"
 else
-	RELEASE_FILE="${BINARIES_DIR}/${BR2_LRD_PRODUCT}-laird.tar"
+	RELEASE_FILE="${BINARIES_DIR}/${BR2_LRD_PRODUCT}-laird.tar.bz2"
 fi
 
-tar -C ${BINARIES_DIR} -rhf ${RELEASE_FILE} \
+tar -C ${BINARIES_DIR} -cjf ${RELEASE_FILE} \
 	--owner=root --group=root \
 	uboot.env mksdcard.sh mksdimg.sh boot.bin u-boot.itb
-
-bzip2 -f ${RELEASE_FILE}
 
 echo "${BR2_LRD_PRODUCT^^} POST IMAGE script: done."
