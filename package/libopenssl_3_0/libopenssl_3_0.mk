1e7b312dc164daa92fd940bc92676265ff228660
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2024-06-02 10:35:30 -0400

    openssl: Fix CVE-2024-4741
    PROD-10894

--- package/libopenssl_3_0/libopenssl_3_0.mk
+++ package/libopenssl_3_0/libopenssl_3_0.mk
@@ -22,9 +22,12 @@ LIBOPENSSL_3_0_CPE_ID_VENDOR = $(LIBOPENSSL_3_0_PROVIDES)
 LIBOPENSSL_3_0_CPE_ID_PRODUCT = $(LIBOPENSSL_3_0_PROVIDES)
 
 ifneq ($(BR2_PACKAGE_LIBOPENSSL_ENABLE_FIPS),y)
-#1002-Check-DSA-parameters-for-excessive-sizes-before-vali.patch
+#0101-Check-DSA-parameters-for-excessive-sizes-before-vali.patch
 LIBOPENSSL_3_0_IGNORE_CVES += CVE-2024-4603
 endif
+
+#0102-Only-free-the-read-buffers-if-we-re-not-using-them.patch
+LIBOPENSSL_3_0_IGNORE_CVES += CVE-2024-4741
 
 ifeq ($(BR2_m68k_cf),y)
 # relocation truncated to fit: R_68K_GOT16O
