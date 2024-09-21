e934384501288aaaf09dc8f8e57aeef443f024b6
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2024-09-21 12:57:34 -0400

    Compatibility with future buildroot
    PROD-14034

--- package/ggv2support/ggv2support.mk
+++ package/ggv2support/ggv2support.mk
@@ -4,19 +4,19 @@
 
 define GGV2SUPPORT_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 -t $(TARGET_DIR)/usr/bin/ \
-		$(GGV2SUPPORT_PKGDIR)ggv2_prepare_sdcard.sh \
+		$(GGV2SUPPORT_PKGDIR)/ggv2_prepare_sdcard.sh \
 		$(GGV2SUPPORT_PKGDIR)/ggv2_mount_sdcard.sh
 	$(INSTALL) -D -m 644 -t $(TARGET_DIR)/etc/modules-load.d \
-		$(GGV2SUPPORT_PKGDIR)f2fs.conf
+		$(GGV2SUPPORT_PKGDIR)/f2fs.conf
 	$(INSTALL) -D -m 644 -t $(TARGET_DIR)/etc/sysctl.d \
-		$(GGV2SUPPORT_PKGDIR)min-free-kbytes.conf
+		$(GGV2SUPPORT_PKGDIR)/min-free-kbytes.conf
 endef
 
 define GGV2SUPPORT_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 644 -t $(TARGET_DIR)/etc/systemd/system \
-		$(GGV2SUPPORT_PKGDIR)ggv2runner.service \
-		$(GGV2SUPPORT_PKGDIR)media-mmcblk0p1-swapfile.swap \
-		$(GGV2SUPPORT_PKGDIR)ggv2sdmount.service
+		$(GGV2SUPPORT_PKGDIR)/ggv2runner.service \
+		$(GGV2SUPPORT_PKGDIR)/media-mmcblk0p1-swapfile.swap \
+		$(GGV2SUPPORT_PKGDIR)/ggv2sdmount.service
 endef
 
 define GGV2SUPPORT_USERS
--- package/summit-edgeiq/summit-edgeiq.mk
+++ package/summit-edgeiq/summit-edgeiq.mk
@@ -4,14 +4,14 @@
 
 define SUMMIT_EDGEIQ_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 -t $(TARGET_DIR)/sbin \
-		$(SUMMIT_EDGEIQ_PKGDIR)adduser \
-		$(SUMMIT_EDGEIQ_PKGDIR)addgroup \
-		$(SUMMIT_EDGEIQ_PKGDIR)factory_reset_edge.sh
+		$(SUMMIT_EDGEIQ_PKGDIR)/adduser \
+		$(SUMMIT_EDGEIQ_PKGDIR)/addgroup \
+		$(SUMMIT_EDGEIQ_PKGDIR)/factory_reset_edge.sh
 endef
 
 define SUMMIT_EDGEIQ_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 644 -t $(TARGET_DIR)$(subst $\",,$(BR2_PACKAGE_SUMMIT_EDGEIQ_INSTALLDIR))/edge/init/systemd \
-		 $(SUMMIT_EDGEIQ_PKGDIR)edge.service
+		 $(SUMMIT_EDGEIQ_PKGDIR)/edge.service
 
 	$(SED) 's/\/TAG_INSTALL_DIR/\$(subst $\",,$(BR2_PACKAGE_SUMMIT_EDGEIQ_INSTALLDIR))/g' \
 		$(TARGET_DIR)$(subst $\",,$(BR2_PACKAGE_SUMMIT_EDGEIQ_INSTALLDIR))/edge/init/systemd/edge.service
@@ -22,7 +22,7 @@ define SUMMIT_EDGEIQ_INSTALL_INIT_SYSTEMD
 		$(TARGET_DIR)/etc/systemd/system/edge.service
 
 	$(INSTALL) -D -m 644 -t $(TARGET_DIR)/etc/systemd/system/tmp.mount.d \
-		 $(SUMMIT_EDGEIQ_PKGDIR)options.conf
+		 $(SUMMIT_EDGEIQ_PKGDIR)/options.conf
 endef
 
 $(eval $(generic-package))
