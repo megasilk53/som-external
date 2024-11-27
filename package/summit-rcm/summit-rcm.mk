e850a316d8e6a65b903ace375550e619f578b939
Chris Trowbridge <chris.trowbridge@ezurio.com>
2024-11-27 10:55:18 -0500

    feat(Summit RCM, live docs): Support uvicorn log level endpoints
    Refs: PROD-15972

--- package/summit-rcm/summit-rcm.mk
+++ package/summit-rcm/summit-rcm.mk
@@ -132,6 +132,7 @@ ifeq ($(BR2_PACKAGE_SUMMIT_RCM_REST_API_DOCS),y)
 		host-python-spectree \
 		host-python-pydantic \
 		host-python-pydantic-core \
+		host-python-uvicorn \
 		host-python-typing-extensions
 	HOST_SUMMIT_RCM_ENV = \
 		DOCS_GENERATION='True' \
