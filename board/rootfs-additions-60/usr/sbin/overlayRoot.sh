6974e849b3169c6b0735b69f9892c4415e84c169
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2025-01-20 15:14:43 -0500

    carbon-am62x: Add initial wbx3 image
    PROD-11253

--- board/rootfs-additions-60/usr/sbin/overlayRoot.sh
+++ board/rootfs-additions-60/usr/sbin/overlayRoot.sh
@@ -14,52 +14,60 @@ ROOT_NEW_RW_MOUNT=${ROOT_NEW_MOUNT}/rw
 [ -z "${rootDevActual}" ] && STANDALONE=true || STANDALONE=false
 
 if ${STANDALONE}; then
+	die () {
+		echo "$@" >&2
+		exit 1
+	}
+
 	# shellcheck source=/dev/null
 	. /usr/sbin/boot-rootfs.sh || die
 fi
 
+[ -d '/sys/module/overlay' ] ||
+	/usr/sbin/modprobe -q overlay || die "ERROR: could not load overlay module"
+
 # create a writable fs to then create our mountpoints
-mount -t tmpfs inittemp /mnt ||
+/usr/bin/mount -t tmpfs inittemp /mnt ||
 	die "ERROR: could not create a temporary filesystem"
 
 mkdir ${ROOT_RO_MOUNT} ${ROOT_RW_MOUNT}
-mount -o noatime -t "${mountFsType:?}" "/dev/$(getPart rootfs_data)" ${ROOT_RW_MOUNT} ||
+/usr/bin/mount -o noatime -t "${mountFsType:?}" "/dev/$(getPart rootfs_data)" ${ROOT_RW_MOUNT} ||
 	die "ERROR: could not create parition for upper filesystem"
 
-mount -t "$(rootMountType)" -o ro "/dev/${rootDev:?}" ${ROOT_RO_MOUNT} ||
+/usr/bin/mount -t "$(rootMountType)" -o ro "/dev/${rootDev:?}" ${ROOT_RO_MOUNT} ||
 	die "ERROR: could not ro mount original root partition"
 
 mkdir -p ${ROOT_RW_MOUNT}/upper ${ROOT_RW_MOUNT}/work ${ROOT_NEW_MOUNT}
-mount -t overlay -o noatime,lowerdir=${ROOT_RO_MOUNT},upperdir=${ROOT_RW_MOUNT}/upper,workdir=${ROOT_RW_MOUNT}/work \
+/usr/bin/mount -t overlay -o noatime,lowerdir=${ROOT_RO_MOUNT},upperdir=${ROOT_RW_MOUNT}/upper,workdir=${ROOT_RW_MOUNT}/work \
 	overlay-rootfs ${ROOT_NEW_MOUNT} ||
 	die "ERROR: could not mount overlayFS"
 
 # remove root mount from fstab
-sed -e '\,/dev/root, s,^,# ,' ${ROOT_RO_MOUNT}/etc/fstab > ${ROOT_NEW_MOUNT}/etc/fstab
+/usr/bin/sed -e '\,/dev/root, s,^,# ,' ${ROOT_RO_MOUNT}/etc/fstab > ${ROOT_NEW_MOUNT}/etc/fstab
 
 # create mount points inside the new rootfs overlay
 mkdir -p ${ROOT_NEW_RO_MOUNT} ${ROOT_NEW_RW_MOUNT}
-mount -n --move ${ROOT_RO_MOUNT} ${ROOT_NEW_RO_MOUNT}
-mount -n --move ${ROOT_RW_MOUNT} ${ROOT_NEW_RW_MOUNT}
+/usr/bin/mount -n --move ${ROOT_RO_MOUNT} ${ROOT_NEW_RO_MOUNT}
+/usr/bin/mount -n --move ${ROOT_RW_MOUNT} ${ROOT_NEW_RW_MOUNT}
 
 # change to the new overlay root
-cd ${ROOT_NEW_MOUNT} || fail "ERROR: could not change to new root"
-pivot_root . ${OVERLAY_ROOT#/}
+cd ${ROOT_NEW_MOUNT} || die "ERROR: could not change to new root"
+/usr/sbin/pivot_root . ${OVERLAY_ROOT#/}
 
-exec chroot . /bin/sh -c "
+exec /usr/sbin/chroot . /bin/sh -c "
 # Move read-only and read-write root file system into the overlay file system
-mount --move ${OVERLAY_ROOT}/proc /proc
-mount --move ${OVERLAY_ROOT}/sys /sys
-mount --move ${OVERLAY_ROOT}/dev /dev
+/usr/bin/mount --move ${OVERLAY_ROOT}/proc /proc
+/usr/bin/mount --move ${OVERLAY_ROOT}/sys /sys
+/usr/bin/mount --move ${OVERLAY_ROOT}/dev /dev
 
 # unmount old read-only root
-umount ${OVERLAY_ROOT}${OVERLAY_ROOT}
-umount ${OVERLAY_ROOT}
+/usr/bin/umount ${OVERLAY_ROOT}${OVERLAY_ROOT}
+/usr/bin/umount ${OVERLAY_ROOT}
 
 if ${STANDALONE}; then
 	# continue with regular init
 	exec /sbin/init
 else
-	exec /sbin/fipsInit.sh restart
+	exec /usr/sbin/fipsInit.sh restart
 fi
 "
