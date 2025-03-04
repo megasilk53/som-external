ca11b593e7b7e490074d218334d25ebcf8221eed
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2025-03-04 10:16:18 -0500

    ftdi-test.py: Added support for CBUS GPIO converted to pyusb
    PROD-11256

--- board/carbon-am62/ftdi-test.py
+++ board/carbon-am62/ftdi-test.py
@@ -23,11 +23,13 @@ if len(sys.argv) > 2:
 else:
     data1 = 0
 
+# Set the direction of the CBUS GPIOs (0 - input, 1 - output)
 data1 = (data1 & 0x0f) | 0x70
 
 BITMODE_BITBANG = 0x01
 BITMODE_CBUS = 0x20
 SIO_SET_BITMODE_REQUEST = 0x0b
+BULK_OUT_ENDPOINT = 0x02
 
 def ftdi_set_bitmode(dev, bitmask, bitmode):
     bmRequestType = usb.util.build_request_type(usb.util.CTRL_OUT,
@@ -37,39 +39,41 @@ def ftdi_set_bitmode(dev, bitmask, bitmode):
     wValue = bitmask | (bitmode << 8)
     dev.ctrl_transfer(bmRequestType, SIO_SET_BITMODE_REQUEST, wValue)
 
-dev = usb.core.find(custom_match = \
-        lambda d: \
-            d.idVendor==0x0403 and
-            d.idProduct==0x6015 and
-            d.product=='FT240X USB FIFO')
+def set_ft240x_gpio(data):
+    dev = usb.core.find(custom_match = \
+            lambda d: \
+                d.idVendor == 0x0403 and
+                d.idProduct == 0x6015 and
+                d.product == 'FT240X USB FIFO')
 
-if not dev:
-    print("FT240X USB FIFO not found.")
-    sys.exit(1)
+    if not dev:
+        print("FT240X USB FIFO not found.")
+        sys.exit(1)
 
-# Remove ttyUSB for this device
-dev.detach_kernel_driver(0)
+    # Remove ttyUSB for this device
+    dev.detach_kernel_driver(0)
 
-#for config in dev:
-#    for i in range(config.bNumInterfaces):
-#        if dev.is_kernel_driver_active(i):
-#            dev.detach_kernel_driver(i)
+    dev.set_configuration()
 
-dev.set_configuration()
+    # Set bit bang mode
+    ftdi_set_bitmode(dev, 0xff, BITMODE_BITBANG)
 
-ftdi_set_bitmode(dev, 0xff, BITMODE_BITBANG)
+    # Write gpios
+    dev.write(BULK_OUT_ENDPOINT, data)
 
-dev.write(0x02, data)
+def set_ft230x_gpio(data):
+    dev = usb.core.find(custom_match = \
+            lambda d: \
+                d.idVendor == 0x0403 and
+                d.idProduct == 0x6015 and
+                d.product == 'FT230X Basic UART')
 
-dev1 = usb.core.find(custom_match = \
-        lambda d: \
-            d.idVendor==0x0403 and
-            d.idProduct==0x6015 and
-            d.product=='FT230X Basic UART')
+    if not dev:
+        print("FT230X Basic UART not found.")
+        sys.exit(1)
 
-if not dev1:
-    print("FT230X Basic UART not found.")
-    sys.exit(1)
+    # Write CBUS gpios
+    ftdi_set_bitmode(dev, data1, BITMODE_CBUS)
 
-
-ftdi_set_bitmode(dev1, data1, BITMODE_CBUS)
+set_ft240x_gpio(data)
+set_ft230x_gpio(data1)
