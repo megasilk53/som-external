871559de5eabd8faf69495399bd68080e5ce667a
Chris Trowbridge <chris.trowbridge@ezurio.com>
2025-04-08 15:29:08 -0400

    chore(echotest): Migrate to PEP517 build
    Refs: PROD-24171

--- package/echotest/echotest.mk
+++ package/echotest/echotest.mk
@@ -7,6 +7,7 @@ ECHOTEST_SITE = $(ECHOTEST_PKGDIR)/source
 ECHOTEST_SITE_METHOD = local
 ECHOTEST_LICENSE = Ezurio
 ECHOTEST_LICENSE_FILES = LICENSE.ezurio
-ECHOTEST_SETUP_TYPE = setuptools
+ECHOTEST_SETUP_TYPE = pep517
+ECHOTEST_DEPENDENCIES = host-python-setuptools
 
 $(eval $(python-package))
