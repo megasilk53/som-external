69693adca5f4e6645c7da3c4c61ec55e0cec2a55
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2025-04-29 06:27:48 -0400

    build-rules.mk: Removed uneeded externals
    PROD-21982

--- build-rules.mk
+++ build-rules.mk
@@ -7,9 +7,6 @@ BR2_EXTERNAL += \
 	$(realpath $(MK_DIR)) \
 	$(call external_name,som-external) \
 	$(call external_name,summit-radio-external) \
-	$(call external_name,summit-radio-devel-external) \
-	$(call external_name,summit-som-mfg-external) \
-	$(call external_name,summit-fips-devel-external) \
 	$(call external_name,vigiles-buildroot)
 
 export BR2_EXTERNAL := $(sort $(BR2_EXTERNAL))
