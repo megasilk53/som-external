2cb4d882453a7d3853d86884de811d3d2e92b4a7
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2025-12-06 11:34:21 -0500

    pkcs-provider: Added hash and cleaneup install
    PROD-46587

--- package-3rd-party/pkcs11-provider/pkcs11-provider.mk
+++ package-3rd-party/pkcs11-provider/pkcs11-provider.mk
@@ -3,9 +3,8 @@
 # pkcs11-provider
 #
 ################################################################################
-PKCS11_PROVIDER_VERSION = 1.1
-PKCS11_PROVIDER_SITE = https://github.com/latchset/pkcs11-provider/releases/download/v$(PKCS11_PROVIDER_VERSION).0
-PKCS11_PROVIDER_SOURCE = pkcs11-provider-$(PKCS11_PROVIDER_VERSION).tar.xz
+PKCS11_PROVIDER_VERSION = 1.1.0
+PKCS11_PROVIDER_SITE = $(call github,latchset,pkcs11-provider,v$(PKCS11_PROVIDER_VERSION))
 PKCS11_PROVIDER_LICENSE = Apache-2.0
 PKCS11_PROVIDER_LICENSE_FILES = LICENSES/Apache-2.0.txt
 PKCS11_PROVIDER_DEPENDENCIES = openssl host-pkgconf p11-kit opensc
@@ -18,7 +17,7 @@ endef
 endif
 
 define PKCS11_PROVIDER_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 -t $(TARGET_DIR)/usr/lib/ossl-modules \
+	$(INSTALL) -D -m 644 -t $(TARGET_DIR)/usr/lib/ossl-modules \
 		$(@D)/build/src/pkcs11.so
 
 	$(INSTALL) -D -m 0644 -t $(TARGET_DIR)/etc/ssl \
